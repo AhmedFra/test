3737963a341b99424c5b0cf0e05f3b8cfc44794f
Anis <anisboudaren2@gmail.com>
2023-05-09 14:56:58 +0100

    stupide bug

--- machyroll/lib/main.dart
+++ machyroll/lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:machyroll/loginPage.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: loginPage(),
     );
   }
 }
