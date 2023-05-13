9b8b765685bb434fe97dde506f7c775daff7dd43
AhmedFra <ahmed.farsi@univ-constantine2.dz>
2023-05-13 04:51:15 +0100

    idk why i spend the last 5h in something wrong gg iam tired

--- machyroll/lib/Figuier_Page.dart
+++ machyroll/lib/Figuier_Page.dart
@@ -1,56 +1,111 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-
+import 'package:machyroll/inventory.dart';
 
 class FiguierPage extends StatelessWidget {
   const FiguierPage({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(186, 8, 8, 8),
-        leading: IconButton(
-        icon: Icon(Icons.arrow_back),
-        onPressed: () {
-           
-      // Handle button press
-      },
-    ),
+        backgroundColor: Colors.transparent,
+        leading:Container(
+          padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(30)
+                                ),
+                                child: Icon(
+                                  Icons.arrow_back_ios_new,
+                                  size: 22,
+                                  color: Colors.white,
+                                ),
 
-        title: Text('Details'),
+        ) ,
       ),
-      body: Stack(
-        children: [
-          Image.asset(
-          'assets/pics/bg_alone.png',
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: double.infinity,
-        ),
-        Container(
-          color: Colors.white,
-          child: Row(
-            children: [
-          
-              CarouselSlider(
-              items: [
-                Image.network('https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwbd5b458b/images/6610071781420-1-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28634693271596.jpg'),
-                Image.network('https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwe04d56bc/images/6610071781420-5-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28718420623404.jpg'),
-                Image.network('https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwa6a01a8d/images/6610071781420-3-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28634348388396.jpg'),
-                Image.network("https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw57a9d791/images/6610071781420-7-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28634102693932.jpg"),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height / 1.7,
+                  viewportFraction: 1.0,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                ),
+                items: [
+                  "https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwbd5b458b/images/6610071781420-1-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28634693271596.jpg",
+                  "https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwe04d56bc/images/6610071781420-5-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28718420623404.jpg",
+                  "https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwa6a01a8d/images/6610071781420-3-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28634348388396.jpg",
+                  "https://store.crunchyroll.com/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw57a9d791/images/6610071781420-7-ultra-tokyo-connection-pvc-scale-figures-demon-slayer-the-movie-mugen-train-akaza-figure-28634102693932.jpg",
+                  // Add more image URLs here
+                ].map((imageUrl) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(imageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    
+                      
+                  );
+                  InkWell(
+
+                  )
+                }).toList(),
+                
+              ),
+              
+            ),
+            SizedBox(height: 10,),
+            Padding(padding: EdgeInsets.only(top :8 ,left :15 ,right : 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(padding: EdgeInsets.only(right:8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "\£29",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.red,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 15),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        child: Icon(
+                          CupertinoIcons.car_fill,
+                          size :22,
+                          color: Colors.red,
+                        ),
+                      ),
+                    )
+                  ],
+                ),)
               ],
-              options: CarouselOptions(
-                height: 400.0,
-                autoPlay: true,
-                enlargeCenterPage: true,
-              ),
-              ),
-            ],
-          ),
+            ),),
+            
+            
+          ],
+            
         ),
-        ]
-      ),
-    );
+        ));
+    
   }
 }
--- machyroll/lib/inventory.dart
+++ machyroll/lib/inventory.dart
@@ -1,4 +1,4 @@
-
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
